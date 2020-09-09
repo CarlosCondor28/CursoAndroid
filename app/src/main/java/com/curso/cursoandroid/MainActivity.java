4619c88a2c6132a6a55e0a5aff4accea3947d6c0
Roberto Carlos Tapia De León <rocatale@ciencias.unam.mx>
2020-09-08 21:33:14 -0500

    Commit 3

--- app/src/main/java/com/curso/cursoandroid/MainActivity.java
+++ app/src/main/java/com/curso/cursoandroid/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button boton;
     private TextView texto;
-
+    private String string;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
